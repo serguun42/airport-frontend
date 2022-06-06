633a98507ae1213f092dfab17597609cfb85dde5
serguun42 <s42@serguun42.ru>
2022-06-06 20:36:08 +0300

    Account methods in API, better app downloading

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,38 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Loading from '../components/Loading';
+import dispatcher from '../util/dispatcher';
+import LogMessageOrError from '../util/log';
 import './Home.css';
 
-export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
+const APP_MIME_TYPE = 'application/vnd.android.package-archive';
+const APP_FILE_TYPE = '.apk';
 
-  useEffect(() => {
-    setTimeout(() => setIsLoading(false), 1000);
-  }, []);
+export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
 
   const DownloadApp = () => {
-    const downloadLink = document.createElement('a');
-    downloadLink.setAttribute('href', process.env.REACT_APP_ANDROID_APP_URL);
-    downloadLink.setAttribute('download', `${process.env.REACT_APP_ANDROID_APP_NAME}.apk`);
-    downloadLink.style.display = 'none';
-    document.body.appendChild(downloadLink);
-    downloadLink.click();
+    dispatcher.call('message', 'Downloading…');
+    setIsLoading(true);
+
+    fetch(process.env.REACT_APP_ANDROID_APP_URL)
+      .then((res) => res.blob())
+      .then((blob) => {
+        const properBlob = blob.slice(0, blob.size, APP_MIME_TYPE);
+        const blobUrl = URL.createObjectURL(properBlob);
+
+        const downloadLink = document.createElement('a');
+        downloadLink.setAttribute('href', blobUrl);
+        downloadLink.setAttribute('download', `${process.env.REACT_APP_ANDROID_APP_NAME}${APP_FILE_TYPE}`);
+        downloadLink.style.display = 'none';
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+      })
+      .catch((e) => {
+        LogMessageOrError(e);
+        dispatcher.call('message', 'Error while downloading');
+      })
+      .finally(() => setIsLoading(false));
   };
 
   return (
